f2cc2a436d78ee3cb402ac717b49540a7cf02d9e
Pablo Gomez alvarez <pgomez-a@c1r14s6.42madrid.com>
2021-02-11 09:42:16 +0100

    commit 10

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pgomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 09:52:58 by pgomez-a          #+#    #+#             */
-/*   Updated: 2021/02/10 14:05:52 by pgomez-a         ###   ########.fr       */
+/*   Updated: 2021/02/11 09:41:16 by pgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,53 +36,64 @@ static int	gnl_look_for_nl(const char *s1)
 	return (0);
 }
 
-static int	gnl_look_in_res(char **res, char *temp, char **line)
+static int	gnl_look_in_res(char **res, char **line)
 {
+	char	*temp;
+	int		verif;
+
 	if (*res == NULL)
 	{
 		*res = gnl_strnew(1);
 		free(*res);
 	}
-	if ((temp = ft_strchr(*res, '\n')))
+	if ((verif = gnl_look_for_nl(*res)) == 1)
 	{
+		temp = ft_strchr(*res, '\n');
 		*line = ft_memccpy(*res, '\n', BUFFER_SIZE);
 		free(*res);
 		*res = ft_strdup(temp);
 		free(temp);
+		temp = NULL;
 		return (1);
 	}
 	*line = ft_strdup(*res);
 	return (0);
 }
 
-/*static void	gnl_buff_nl(char *buff, char **res, char *temp, char **line)
+static void	gnl_buff_nl(char *buff, int prov, char **res, char **line)
 {
+	char	*temp;
+
 	temp = ft_memccpy(buff, '\n', BUFFER_SIZE);
 	*line = ft_strjoin(*line, temp);
 	free(temp);
+	temp = NULL;
+	if (prov == 1)
+		free(*res);
 	*res = ft_strchr(buff, '\n');
-}*/
+}
 
 int			get_next_line(int fd, char **line)
 {
 	static char	*res;
-	char		*temp;
 	char		buff[BUFFER_SIZE + 1];
 	int			verif;
 	int			num;
+	int			prov;
 
+	prov = 1;
+	if (res == NULL)
+		prov = 0;
 	if (!(fd >= 0 && fd < 123) || BUFFER_SIZE <= 0 || !line)
 		return (-1);
-	temp = NULL;
-	if ((num = gnl_look_in_res(&res, temp, line)) == 1)
+	if ((num = gnl_look_in_res(&res, line)) == 1)
 		return (1);
 	while ((num = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		buff[num] = '\0';
 		if ((verif = gnl_look_for_nl(buff)) == 1)
 		{
-			//gnl_buff_nl(buff, &res, temp, line);
-			temp = NULL;
+			gnl_buff_nl(buff, prov, &res, line);
 			return (1);
 		}
 		*line = ft_strjoin(*line, buff);
--- main.c
+++ main.c
@@ -6,33 +6,34 @@
 /*   By: pgomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 17:15:20 by pgomez-a          #+#    #+#             */
-/*   Updated: 2021/02/10 13:52:35 by pgomez-a         ###   ########.fr       */
+/*   Updated: 2021/02/11 09:37:35 by pgomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
-int main()
-{
-    int fd;
-    int r;
-    char *line = 0;
-    int i = 0;
 
-    fd = 0;
-    fd = open("t1_consalto.txt", O_RDONLY);
-    r = 1;
-    while (r == 1)
-    {
-        r = get_next_line(fd, &line);
-        printf("\n%s\n", line);
-        if (line)
-            free(line);
-        i++;
-    }
-    close(fd);
-    printf ("Termina, valor de r final: %i\n", r);
-    system("leaks a.out");
-    return (0);
+int main(void)
+{
+	int fd;
+	int r;
+	char *line = 0;
+	int i = 0;
+
+	fd = 0;
+	fd = open("t1_consalto.txt", O_RDONLY);
+	r = 1;
+	while (r == 1)
+	{
+		r = get_next_line(fd, &line);
+		printf("\n%s\n", line);
+		if (line)
+			free(line);
+		i++;
+	}
+	close(fd);
+	//printf ("Termina, valor de r final: %i\n", r);
+	system("leaks a.out");
+	return (0);
 }
